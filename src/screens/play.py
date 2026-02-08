66f63c4d449e88ca443c49991c77ac62cdbbbfc5
Brandan Stradling <brandan.stradling@gmail.com>
2026-02-08 17:22:43 -0500

    Add pause toggle in PlayScreen (P to pause/resume)

--- src/screens/play.py
+++ src/screens/play.py
@@ -1,5 +1,8 @@
+import pygame
+
+from src.constants import WIDTH, HEIGHT
 from src.game import Game
-from src.ui import draw_status
+from src.ui import draw_status, draw_text
 
 class PlayScreen:
     def __init__(self, app, screen, sounds, music):
@@ -8,8 +11,28 @@ class PlayScreen:
         self.sounds = sounds
         self.music = music
         self.game = Game(with_player=True, sounds=sounds)
+        self.paused = False
 
     def update(self, input_state):
+        if input_state.pause_pressed:
+            self.paused = not self.paused
+
+            try:    
+                if self.paused:
+                    self.music.pause()
+                else:
+                    self.music.unpause()
+
+            except Exception:
+                if self.paused:
+                    self.music.stop()
+                else:
+                    self.music.play("theme")
+            return
+        
+        if self.paused:
+            return
+        
         self.game.update(input_state)
         if self.game.player.lives < 0:
             self.game.play_sound("over")
@@ -19,3 +42,10 @@ class PlayScreen:
     def draw(self):
         self.game.draw(self.screen)
         draw_status(self.screen, self.game)
+
+        if self.paused:
+            overlay = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
+            overlay.fill((0, 0, 0, 140))
+            self.screen.surface.blit(overlay, (0, 0))
+
+            draw_text(self.screen, "PAUSED", 200) 
